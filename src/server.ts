c11e180a867de89cd6142f3f4230d5a3ff3ac333
hugosrc <hugo.uibai54@gmail.com>
2020-08-08 11:28:21 -0300

    feat: create not found page

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import path from 'path'
 import nunjucks from 'nunjucks'
 
@@ -17,6 +17,10 @@ nunjucks.configure('src/views', {
 
 app.use(express.json())
 app.use(routes)
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  return res.render('not-found.html')
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
